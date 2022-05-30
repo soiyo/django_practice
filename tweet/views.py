0ca6fb02ff31ce405a430d150af3a2689b496b2d
soiyo <soiyosauce@gmail.com>
2022-05-30 20:55:04 +0900

    05-03

--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,5 @@
+from enum import auto
+import re
 from .models import TweetComment, TweetModel  # 글쓰기모델 : 가장 윗부분에 적어주기
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
@@ -53,22 +55,18 @@ def home(request):
         return redirect("/sign-in")
 
 
+@login_required
 def tweet(request):
     if request.method == "GET":
-        user = request.user.is_authenticated  # 사용자가 로그인이 되어 있는지 확인하기
-        if user:  # 로그인한 사용자라면
-            all_tweet = TweetModel.objects.all().order_by("-created_at")
-            # TweetModel을 created_at의 역순으로 불러오는 코드
-            return render(request, "tweet/home.html", {"tweet": all_tweet})
-            # tweet/home.html을 화면에 띄우면서 {'tweet':all_tweet} 라는 데이터를 화면에 전달한다
-        else:
-            return redirect("/sign-in")
+        all_tweet = TweetModel.objects.all().order_by("-created_at")
+        return render(request, "tweet/home.html", {"tweet": all_tweet})
     elif request.method == "POST":
-        user = request.user  # 현재 로그인 한 사용자를 불러오기
-        my_tweet = TweetModel()  # 글쓰기 모델 가져오기
-        my_tweet.author = user  # 모델에 사용자 저장
-        my_tweet.content = request.POST.get(
-            "my-content", ""
-        )  # 모델에 글 저장, html에서 id와 name이 'my-content'였음
-        my_tweet.save()
-        return redirect("/tweet")
+        user = request.user
+        content = request.POST.get("my-content", "")
+        if content == "":
+            all_tweet = TweetModel.objects.all().order_by("-created_at")
+            return render(request, "tweet/home.html", {"error": "글은 공백일 수 없습니다."})
+        else:
+            my_tweet = TweetModel.objects.create(author=user, content=content)
+            my_tweet.save()
+            return redirect("/tweet")
