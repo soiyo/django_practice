1ff8027c0fa931fbe57d91272e2c58b8391ba110
soiyo <soiyosauce@gmail.com>
2022-05-30 17:26:53 +0900

    03-11

--- tweet/views.py
+++ tweet/views.py
@@ -1,4 +1,6 @@
 import re
+from wsgiref.util import request_uri
+from .models import TweetModel  # 글쓰기모델 : 가장 윗부분에 적어주기
 from django.shortcuts import render, redirect
 
 # Create your views here.
@@ -14,6 +16,18 @@ def tweet(request):
     if request.method == "GET":
         user = request.user.is_authenticated  # 사용자가 로그인이 되어 있는지 확인하기
         if user:  # 로그인한 사용자라면
-            return render(request, "tweet/home.html")
+            all_tweet = TweetModel.objects.all().order_by("-created_at")
+            # TweetModel을 created_at의 역순으로 불러오는 코드
+            return render(request, "tweet/home.html", {"tweet": all_tweet})
+            # tweet/home.html을 화면에 띄우면서 {'tweet':all_tweet} 라는 데이터를 화면에 전달한다
         else:
             return redirect("/sign-in")
+    elif request.method == "POST":
+        user = request.user  # 현재 로그인 한 사용자를 불러오기
+        my_tweet = TweetModel()  # 글쓰기 모델 가져오기
+        my_tweet.author = user  # 모델에 사용자 저장
+        my_tweet.content = request.POST.get(
+            "my-content", ""
+        )  # 모델에 글 저장, html에서 id와 name이 'my-content'였음
+        my_tweet.save()
+        return redirect("/tweet")
