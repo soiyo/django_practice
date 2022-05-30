be614662e9001225e02de5520988b86d31ef1da4
soiyo <soiyosauce@gmail.com>
2022-05-30 15:47:43 +0900

    02-09

--- django_practice/views.py
+++ django_practice/views.py
@@ -1,4 +1,3 @@
-import imp
 from django.http import HttpResponse
 from django.shortcuts import render
 
--- user/views.py
+++ user/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import UserModel
+from django.http import HttpResponse
 
 # Create your views here.
 def sign_up_view(request):
-    return render(request, "user/signup.html")
+    if request.method == "GET":
+        return render(request, "user/signup.html")
+    elif request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+        password2 = request.POST.get("password2", None)
+        bio = request.POST.get("bio", None)
+
+        if password != password2:
+            return render(request, "user/signup.html")
+        else:
+            new_user = UserModel()
+            new_user.username = username
+            new_user.password = password
+            new_user.bio = bio
+            new_user.save()
+        return redirect("/sign-in")
 
 
 def sign_in_view(request):
-    return render(request, "user/signin.html")
+    if request.method == "POST":
+        username = request.POST.get("username", None)
+        password = request.POST.get("password", None)
+
+        me = UserModel.objects.get(username=username)  # 사용자 불러오기
+        if me.password == password:
+            request.session["user"] = me.username  # 세션에 사용자 이름 저장
+            return HttpResponse("로그인 성공!")
+        else:
+            return redirect("/sign-in")
+    if request.method == "GET":
+        return render(request, "user/signin.html")
