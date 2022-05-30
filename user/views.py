819ba6ac572aa48de1163ad7f90dcf541a0dd9b1
soiyo <soiyosauce@gmail.com>
2022-05-30 15:50:39 +0900

    02-HW

--- user/views.py
+++ user/views.py
@@ -1,4 +1,7 @@
+from pickletools import read_uint1
 from django.shortcuts import render, redirect
+
+import user
 from .models import UserModel
 from django.http import HttpResponse
 
@@ -15,12 +18,18 @@ def sign_up_view(request):
         if password != password2:
             return render(request, "user/signup.html")
         else:
-            new_user = UserModel()
-            new_user.username = username
-            new_user.password = password
-            new_user.bio = bio
-            new_user.save()
-        return redirect("/sign-in")
+            # 아이디 중복 체크
+            exist_user = UserModel.objects.filter(username=username)
+
+            if exist_user:
+                return render(request, "user/signup.html")
+            else:
+                new_user = UserModel()
+                new_user.username = username
+                new_user.password = password
+                new_user.bio = bio
+                new_user.save()
+                return redirect("/sign-in")
 
 
 def sign_in_view(request):
@@ -31,7 +40,7 @@ def sign_in_view(request):
         me = UserModel.objects.get(username=username)  # 사용자 불러오기
         if me.password == password:
             request.session["user"] = me.username  # 세션에 사용자 이름 저장
-            return HttpResponse("로그인 성공!")
+            return HttpResponse(me.username)  # 로그인 성공시 사용자 이름 출력
         else:
             return redirect("/sign-in")
     if request.method == "GET":
