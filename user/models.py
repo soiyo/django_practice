3955fcf39b88f7bdf9509ffb1bdc24dc02974c54
soiyo <soiyosauce@gmail.com>
2022-05-30 14:50:11 +0900

    02-05

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
+class UserModel(models.Model):
+    class Meta:
+        db_table = "my_user"
+
+    username = models.CharField(max_length=20, null=False)
+    password = models.CharField(max_length=256, null=False)
+    bio = models.CharField(max_length=256, default="")
+    create_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
